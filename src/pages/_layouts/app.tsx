06cb92b008e87b6fe0cb2d5fa17b8b93da714605
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-07-04 21:26:21 -0300

    feat: create header and navlink componenent

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,20 +1,11 @@
-import { Pizza } from "lucide-react";
+import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg text-foreground">
-          <Pizza className="h-5 w-5" />
-          <span className="font-semibold">pizza.shop</span>
-        </div>
-        <footer className="text-sm">
-          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
-        </footer>
-      </div>
-
-      <div className="flex flex-col items-center">
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,25 +6,43 @@ import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { Link } from "react-router-dom";
+import { toast } from "sonner";
+
 const signInForm = z.object({
   email: z.string().email(),
 });
 
-type SignInFormData = z.infer<typeof SignInFormData>;
+type SignInFormData = z.infer<typeof signInForm>;
 
 export function SignIn() {
-  const { register, handleSubmit, formState: {isSubmitting} } = useForm<SignInFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
     resolver: zodResolver(signInForm),
   });
 
   async function handleSignIn(data: SignInFormData) {
     console.log(data);
+    toast.success("Enviamos um link de autenticao para o seu e-mail.", {
+      action: {
+        label: "Reenviar",
+        onClick: () => handleSignIn(data),
+      },
+    });
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild>
+          <Link to="/sign-up" className="absolute right-8 top-8">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -38,14 +56,11 @@ export function SignIn() {
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="Digite seu e-mail"
-                {...register("email")}
-              />
+              <Input id="email" type="email" {...register("email")} />
             </div>
-            <Button className="w-full" disabled={isSubmitting}>Acessar painel</Button>
+            <Button className="w-full" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
